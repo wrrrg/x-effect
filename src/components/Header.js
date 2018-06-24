8207ccf906819f44b72f3da03823837cebe974f4
wrrrg <bill.gryta@gmail.com>
2018-06-23 21:25:44 -0500

    functional X button with corresponding message

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <div className="header">
+    <h1 className="header__title">Did you meet your goal today?</h1>
+  </div>
+);
+
+export { Header };
---
+++ src/components/SingleX.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SingleX = props => (
+  <div>
+    <button onClick={props.handleXClick}>X</button>
+  </div>
+);
+
+export default SingleX;
