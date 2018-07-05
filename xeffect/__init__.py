7dc801cceb8c8f3478bdc26a91043b88f8608e13
Colty Harrison <coltyharrison@gmail.com>
2018-07-04 23:01:42 -0700

    Fix bugs in login process and Add logger

--- xeffect/__init__.py
+++ xeffect/__init__.py
@@ -1,4 +1,5 @@
 import os
+from logging.config import dictConfig
 
 from . import config
 from .auth.views import auth_bp
@@ -8,6 +9,21 @@ from flask_cli import FlaskCLI
 
 
 def create_app():
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
     app = Flask(__name__, template_folder="../templates",
                 static_folder="../static")
     FlaskCLI(app)
